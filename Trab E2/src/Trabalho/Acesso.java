40943a29bfb437d96e412dcae9a7c71cfcfb1c1a
johnaopedro <87997969+johnaopedro@users.noreply.github.com>
2023-06-12 18:27:55 -0300

    Update Acesso.java

--- Trab E2/src/Trabalho/Acesso.java
+++ Trab E2/src/Trabalho/Acesso.java
@@ -1,5 +1,12 @@
 package Trabalho;
 
 public class Acesso {
+  	public static void main(String[] args) {
+		
+		ArrayList<Estacionamento> Estacionamentos = new ArrayList<>();
+		ArrayList<Acesso> Acessos = new ArrayList<>();
+		
+		
+	}
 
 }
